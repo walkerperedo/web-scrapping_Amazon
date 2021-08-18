678b9b0849e0e34d9c0babc566398a6bbc805daf
Walker Peredo <walkerperedo@gmail.com>
2021-08-17 22:47:50 -0400

    selecting all elements of results

--- index.js
+++ index.js
@@ -8,4 +8,23 @@ const path = require("path");
   await page.goto("https://www.amazon.es");
   await page.waitForSelector("#twotabsearchtextbox");
   await page.type("#twotabsearchtextbox", "web development");
+  await page.waitForSelector("#nav-search-submit-button");
+  await page.click("#nav-search-submit-button");
+  await page.waitForSelector("[data-component-type=s-search-result]");
+
+  const infoText = await page.evaluate(() => {
+    const info = document.querySelectorAll(
+      "[data-component-type=s-search-result]"
+    );
+
+    const infoText = [];
+
+    for (const el of info) {
+      infoText.push(el);
+    }
+    console.log(infoText);
+
+    return infoText;
+  });
+  console.log(infoText);
 })();
