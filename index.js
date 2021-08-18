40cfc4affa9de00604746fffb13c1e0d4777495b
Walker Peredo <walkerperedo@gmail.com>
2021-08-17 22:20:19 -0400

    select search bar and write

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const puppeteer = require("puppeteer");
+const path = require("path");
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+
+  const page = await browser.newPage();
+  await page.goto("https://www.amazon.es");
+  await page.waitForSelector("#twotabsearchtextbox");
+  await page.type("#twotabsearchtextbox", "web development");
+})();
